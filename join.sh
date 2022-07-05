ace638adf141a37b654d32c4b3bfb53400af625d
ekazfarm <syachferdian4@gmail.com>
2022-07-06 00:24:57 +0700

    Update join.sh

--- join.sh
+++ join.sh
@@ -2,6 +2,7 @@
 
 echo "leave realm"
 realm leave indodana.id
+realm leave soltech.local
 
 echo "Install Paket Yang dibutuhkan"
 yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
